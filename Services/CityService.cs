ba7a053583d15ab3d1a403bf5bdd9a6eb35dd936
nicolasbalduino <nicolasbalduino@outlook.com>
2023-04-22 18:01:59 -0300

    retirado os codigos comentados da service de cidade

--- Services/CityService.cs
+++ Services/CityService.cs
@@ -22,82 +22,26 @@ namespace Services
         public List<City> FindAll()
         {
             return cityRepository.FindAll();
-            //List<City> cities = new();
-
-            //StringBuilder sb = new StringBuilder();
-            //sb.Append("SELECT c.Description");
-            //sb.Append(" FROM City c");
-
-            //SqlCommand commandSelect = new(sb.ToString(), conn);
-            //SqlDataReader dr = commandSelect.ExecuteReader();
-
-            //while (dr.Read())
-            //{
-            //    City showCity = new();
-
-            //    showCity.Description = (string)dr["Description"];
-
-
-            //    cities.Add(showCity);
-            //}
-            return new(); // cities;
         }
 
         public City FindByName(string name)
         {
             return cityRepository.FindByName(name);
-            //string strSelect = $"SELECT c.Id, c.Description FROM City c WHERE c.Description = '{name}';";
-            //SqlCommand commandSelect = new(strSelect, conn);
-            //SqlDataReader dr = commandSelect.ExecuteReader();
-
-            //City city = new();
-
-            //if (dr.Read())
-            //{
-            //    city.Id = (int)dr["Id"];
-            //    city.Description = (string)dr["Description"];
-            //}
-
-            return new();// city;
         }
 
         public City FindById(int id)
         {
             return cityRepository.FindById(id);
-            //string strSelect = $"SELECT c.Id, c.Description FROM City c WHERE c.Id = {id};";
-            //SqlCommand commandSelect = new(strSelect, conn);
-            //SqlDataReader dr = commandSelect.ExecuteReader();
-
-            //City city = new();
-            //if (dr.Read())
-            //{
-            //    city.Id = (int)dr["Id"];
-            //    city.Description = (string)dr["Description"];
-            //}
-
-            return new(); // city;
         }
 
         public int UpdateCity(string name, string newName)
         {
             return cityRepository.Update(name, newName);
-            //City cityToEdit = new CityService().FindByName(name);
-            //if (cityToEdit == null)
-            //    return 0;
-
-            //string strUpdate = $"UPDATE City SET Description = '{newName}' WHERE Id = {cityToEdit.Id};";
-            //SqlCommand commandUpdate = new(strUpdate, conn);
-            //var result = commandUpdate.ExecuteNonQuery();
-            return 0; // result;
         }
 
         public int Delete(int id)
         {
             return cityRepository.Delete(id);
-            //string strDelete = $"DELETE FROM City WHERE Id = {id};";
-            //SqlCommand commandDelete = new(strDelete, conn);
-            //var result = commandDelete.ExecuteNonQuery();
-            return 0; // result;
         }
     }
 }
